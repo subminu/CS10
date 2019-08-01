cd02f5f4c66d046550a88327bd23aab3b81012d7
subminu <subminu@gmail.com>
2019-08-01 10:19:34 +0900

    Ver 0.5

--- ThanosVSIronMan.py
+++ ThanosVSIronMan.py
@@ -1,97 +1,8 @@
-# Listing_10-1.py
-# Copyright Warren & Carter Sande, 2013
-# Released under MIT license   http://www.opensource.org/licenses/mit-license.php
-# Version $version  ----------------------------
-
-# Skier program
-
 import pygame, sys, os, random
 import time
 
-# different images for the skier depending on his direction
-"""
-start = time. time()
-"the code you want to test stays here"
-end = time. time()
-print(end - start)
-"""
-
-
-def load_images():
-    folder = 'images/'
-    image_names = []
-
-    for filename in os.listdir(folder):
-        if any([filename.endswith(x) for x in ['.png']]):
-            img_name = os.path.join(filename)
-            if not filename.startswith(('IronMan_crash.png', 'IronMan_nuclear.png', 'meteoroid.png')):
-                image_names.append(img_name)
-
-    return image_names
-
-
-def sort_images():
-    sorted_images = ["IronMan_up.png", "IronMan_right1.png", "IronMan_right2.png", "IronMan_left2.png", "IronMan_left1.png"]
-    '''unsorted_images = load_images()
-    order_map = {}
-    for pos, item in enumerate(image_order):
-        order_map[item] = pos
-
-    sorted_images = sorted(unsorted_images, key=order_map.get)
-    print(sorted_images)'''
-    return sorted_images
-
-
-skier_images = sort_images()
-
-"""
-class GetImages(object):
-
-    def __init__(self):
-        self.folder = 'images/'
-        self.image_order = ["skier_down.png", "skier_right1.png", "skier_right2.png","skier_left2.png", "skier_left1.png"]
-        self.order_map = {}
-        
-        
-
-    def load_images(self):
-        self.image_names = []
-        for filename in os.listdir(self.folder):
-            if any([filename.endswith(x) for x in ['.png']]):
-                img_name = os.path.join(filename)
-                if not filename.startswith(('skier_crash.png', 'skier_flag.png', 'skier_tree.png')):
-                    self.image_names.append(img_name)
-
-        return self.image_names
-
-    def sort_images(self):
-        #self.sorted_images = self.load_images()
-        
-        for pos, item in enumerate(self.image_order):
-            self.order_map[item] = pos
-
-        #self.sorted_images = self.load_images(), self.sorted_images.sort(key=self.order_map.get)
-        self.loaded_images = self.load_images()
-        self.sorted_images = sorted(self.loaded_images, key=self.order_map.get)
-        
-        return self.sorted_images
-
-
-#Sorted skier_images
-#Simages = GetImages()
-
-print(GetImages().load_images())
-print(GetImages().sort_images())
-skier_images1 = GetImages().sort_images()
-print(skier_images1)
-end = time. time()
-print(end - start)
-
-"""
-
-
-# class for the Skier sprite
-class SkierClass(pygame.sprite.Sprite):
+# class for the IronMan sprite
+class IronManClass(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load("IronMan_up.png")
@@ -101,26 +12,25 @@ class SkierClass(pygame.sprite.Sprite):
         self.heart = 3
 
     def turn(self, direction):
-        # load new image and change speed when the skier turns
+        # load new image and change speed when the IronMan turns
         self.angle = self.angle + direction
         if self.angle < -2:  self.angle = -2
         if self.angle > 2:  self.angle = 2
         center = self.rect.center
-        self.image = pygame.image.load(skier_images[self.angle])
+        self.image = pygame.image.load(IronMan_images[self.angle])
         self.rect = self.image.get_rect()
         self.rect.center = center
         speed = [self.angle, 10 - abs(self.angle) * 2]
         return speed
 
     def move(self, speed):
-        # move the skier right and left
-        self.rect.centerx = self.rect.centerx + speed[0]
+        # move the IronMan right and left
+        self.rect.centerx = self.rect.centerx + speed[0] * 3
         if self.rect.centerx < 20:  self.rect.centerx = 20
         if self.rect.centerx > 620: self.rect.centerx = 620
 
-    # class for obstacle sprites (trees and flags)
 
-
+# class for obstacle sprites (meteoroids and nuclears)
 class ObstacleClass(pygame.sprite.Sprite):
     def __init__(self, image_file, location, type):
         pygame.sprite.Sprite.__init__(self)
@@ -149,7 +59,7 @@ def create_map():
         location = [col * 64 + 32 , row * 64 + 32 - 640]  # center x, y for obstacle
         if not (location in locations):  # prevent 2 obstacles in the same place
             locations.append(location)
-            type = random.choice(["meteoroid", "power"])
+            type = random.choice(["meteoroid", "meteoroid", "power"]) # Make more meteoroids displayed on screen
             if type == "meteoroid":
                 img = "meteoroid.png"
             elif type == "power":
@@ -162,7 +72,7 @@ def create_map():
 def animate():
     screen.fill([0, 0, 0])
     obstacles.draw(screen)
-    screen.blit(skier.image, skier.rect)
+    screen.blit(IronMan.image, IronMan.rect)
     screen.blit(score_text, [10, 10])
     pygame.display.flip()
 
@@ -171,10 +81,11 @@ def animate():
 pygame.init()
 screen = pygame.display.set_mode([640, 640])
 pygame.display.set_caption("Thanos VS IronMan")
+IronMan_images = ["IronMan_up.png", "IronMan_right1.png", "IronMan_right2.png", "IronMan_left2.png", "IronMan_left1.png"]
 clock = pygame.time.Clock()
 speed = [0, 10]
 obstacles = pygame.sprite.Group()  # group of obstacle objects
-skier = SkierClass()
+IronMan = IronManClass()
 map_position = 0
 points = 0
 create_map()  # create one screen full of obstacles
@@ -184,17 +95,17 @@ font = pygame.font.Font(None, 50)
 running = True
 while running:
     clock.tick(30)
-    if skier.heart<=0:
+    if IronMan.heart<=0:
         running=False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:  # check for key presses
             if event.key == pygame.K_LEFT:  # left arrow turns left
-                speed = skier.turn(-1)
+                speed = IronMan.turn(-1)
             elif event.key == pygame.K_RIGHT:  # right arrow turns right
-                speed = skier.turn(1)
-    skier.move(speed)  # move the skier (left or right)
+                speed = IronMan.turn(1)
+    IronMan.move(speed)  # move the IronMan (left or right)
     map_position -= speed[1]  # scroll the obstacles
 
     # create a new block of obstacles at the bottom
@@ -202,22 +113,22 @@ while running:
         create_map()
         map_position = 0
 
-    # check for hitting trees or getting flags
-    hit = pygame.sprite.spritecollide(skier, obstacles, False)
+    # check for hitting meteoroids or getting nuclears
+    hit = pygame.sprite.spritecollide(IronMan, obstacles, False)
     if hit:
-        if hit[0].type == "meteoroid" and not hit[0].passed:  # crashed into tree
+        if hit[0].type == "meteoroid" and not hit[0].passed:  # crashed into meteoroids
             points = points - 50
-            skier.image = pygame.image.load("IronMan_crash.png")  # crash image
+            IronMan.image = pygame.image.load("IronMan_crash.png")  # crash image
             animate()
             pygame.time.delay(1000)
-            skier.image = pygame.image.load("IronMan_up.png")  # resume skiing
-            skier.angle = 0
-            speed = [0, 6]
+            IronMan.image = pygame.image.load("IronMan_up.png")  # resume skiing
+            IronMan.angle = 0
+            speed = [0, 10]
             hit[0].passed = True
-            skier.heart -=1
-        elif hit[0].type == "power" and not hit[0].passed:  # got a flag
+            IronMan.heart -=1
+        elif hit[0].type == "power" and not hit[0].passed:  # got a nuclear
             points += 10
-            hit[0].kill()  # remove the flag
+            hit[0].kill()  # remove the nuclear
 
     obstacles.update()
     score_text = font.render("Score: " + str(points), 1, (255, 255, 255))
